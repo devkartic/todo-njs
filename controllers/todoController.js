04db07a5183370455c1b14e46b1eea0409a3c670
dev.kartic <dev.kartic@gmail.com>
2020-05-11 21:08:44 +0600

    Create mongodb connection, model and save into database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,24 @@
 let data = [{item: 'Get Milk'}, {item: 'Walk dog',},{ item: 'Kick some coding ass'}];
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Connection to database mongodb
+mongoose.connect('mongodb+srv://root:root@clustertodo-ohuuo.mongodb.net/test?retryWrites=true&w=majority');
+
+// Create a schema- This like a blue print
+let todoSchema = new mongoose.Schema({
+    item : String
+});
+
+// Create Model
+let Todo = mongoose.model('Todo', todoSchema)
+
+// Save data with exception and success message
+let itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if (err) throw err;
+    console.log('item saved');
+});
+
 // Use body-parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
